b16ea03c679cdf90f4bbdb2ac6adb0ae1c71dd3a
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-01 11:34:24 +0700

    Menambahkan auto-create direktori storage di tmp

--- api/index.php
+++ api/index.php
@@ -1,2 +1,21 @@
 <?php
+
+// Daftar folder yang dibutuhkan Laravel di dalam /tmp Vercel
+$directories = [
+    '/tmp/app',
+    '/tmp/framework/cache/data',
+    '/tmp/framework/sessions',
+    '/tmp/framework/testing',
+    '/tmp/framework/views',
+    '/tmp/logs'
+];
+
+// Buat foldernya jika belum ada
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+// Panggil file index asli Laravel
 require __DIR__ . '/../public/index.php';
