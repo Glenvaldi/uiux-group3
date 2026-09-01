98973b9639e762d63e522f6612e0ede4f97adc1b
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-01 23:12:12 +0700

    Fix final: Gabungan bypass cache dan force driver

--- api/index.php
+++ api/index.php
@@ -1,11 +1,27 @@
 <?php
 
-// JURUS PAMUNGKAS: Mengalihkan folder bootstrap/cache ke /tmp Vercel
+// 1. BYPASS FOLDER CACHE KE /TMP
 $_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
 $_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
 $_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 $_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
 $_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
 
-// Panggil file index asli Laravel
+// 2. PAKSA DRIVER AGAR TIDAK KOSONG (Mencegah error createDriver)
+$envs = [
+    'SESSION_DRIVER' => 'cookie',
+    'CACHE_STORE' => 'array',
+    'CACHE_DRIVER' => 'array',
+    'LOG_CHANNEL' => 'stderr',
+    'DB_CONNECTION' => 'sqlite',
+    'DB_DATABASE' => '/tmp/database.sqlite'
+];
+
+foreach ($envs as $key => $value) {
+    putenv($key . '=' . $value);
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
+
+// 3. PANGGIL LARAVEL
 require __DIR__ . '/../public/index.php';
