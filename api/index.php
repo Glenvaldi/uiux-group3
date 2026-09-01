52842477202c76ff5be09ca90b194b71af163c84
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-01 12:09:20 +0700

    Mengaktifkan pencarian error PHP

--- api/index.php
+++ api/index.php
@@ -1,2 +1,9 @@
 <?php
+
+// Paksa PHP untuk menampilkan error fatal di layar
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Panggil file index asli Laravel
 require __DIR__ . '/../public/index.php';
