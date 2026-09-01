b24406bf526e90de7dc24fb78bbc54bd122271af
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-01 10:51:07 +0700

    Fix Vercel read-only storage issue

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
 <?php
+
+// Mengarahkan file cache sementara ke folder /tmp milik Vercel
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=cookie');
+
+// Memanggil file index asli Laravel
 require __DIR__ . '/../public/index.php';
