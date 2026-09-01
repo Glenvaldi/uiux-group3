5a91b99efda6904fc7c50b39f12eda997dd504fb
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-01 11:36:45 +0700

    Menerapkan God Mode api/index.php untuk Vercel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,18 @@
 <?php
 
-// Daftar folder yang dibutuhkan Laravel di dalam /tmp Vercel
+// 1. PAKSA PENGATURAN (Bypass Vercel Environment Variables)
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/framework/views';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['CACHE_STORE'] = 'array'; 
+$_ENV['CACHE_DRIVER'] = 'array'; 
+$_ENV['LOG_CHANNEL'] = 'stderr';
+
+// 2. BUAT FOLDER PENAMPUNGAN OTOMATIS
 $directories = [
     '/tmp/app',
     '/tmp/framework/cache/data',
@@ -10,12 +22,11 @@ $directories = [
     '/tmp/logs'
 ];
 
-// Buat foldernya jika belum ada
 foreach ($directories as $dir) {
     if (!is_dir($dir)) {
         mkdir($dir, 0777, true);
     }
 }
 
-// Panggil file index asli Laravel
+// 3. PANGGIL LARAVEL
 require __DIR__ . '/../public/index.php';
