a849a8963bca8474881726552413094abc09673a
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-01 22:46:40 +0700

    Memasang kembali jaring penangkap error

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
-require __DIR__ . '/../public/index.php';
+// 1. Nyalakan semua radar error PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// 2. Pasang jaring penangkap Laravel
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1 style='color:red;'>ERROR TERTANGKAP!</h1>";
+    echo "<b>Pesan Error:</b> " . $e->getMessage() . "<br><br>";
+    echo "<b>Terjadi di File:</b> " . $e->getFile() . "<br>";
+    echo "<b>Pada Baris:</b> " . $e->getLine() . "<br>";
+}
