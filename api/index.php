2123215f69dafa27098c76264109c0d1e19a4c6a
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-01 23:10:02 +0700

    Mengalahkan bos terakhir: bypass bootstrap cache ke tmp

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
+
+// JURUS PAMUNGKAS: Mengalihkan folder bootstrap/cache ke /tmp Vercel
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+
+// Panggil file index asli Laravel
 require __DIR__ . '/../public/index.php';
