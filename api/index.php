ad74ff9c967554544381d9bd009376d205a3bb94
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-01 22:44:13 +0700

    Menghapus total file cache lokal yang beracun

--- api/index.php
+++ api/index.php
@@ -1,13 +1,2 @@
 <?php
-
-try {
-    // Kita coba bangunkan Laravel
-    require __DIR__ . '/../public/index.php';
-    
-} catch (\Throwable $e) {
-    // Kalau Laravel pingsan/error, tangkap alasannya dan tampilkan!
-    echo "<h1 style='color:red;'>LARAVEL ERROR KETANGKAP!</h1>";
-    echo "<b>Pesan Error:</b> " . $e->getMessage() . "<br><br>";
-    echo "<b>Terjadi di File:</b> " . $e->getFile() . "<br>";
-    echo "<b>Pada Baris:</b> " . $e->getLine() . "<br>";
-}
+require __DIR__ . '/../public/index.php';
