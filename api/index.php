88e35f3c45a0cbfa7822b659035c5ab3d8ac1af3
Glenvaldi Felix Tantoko <glenvaldi@gmail.com>
2026-09-02 10:48:16 +0700

    Fix bug driver kosong di Maintenance Mode

--- api/index.php
+++ api/index.php
@@ -1,27 +1,16 @@
 <?php
 
-// 1. BYPASS FOLDER CACHE KE /TMP
+// 1. MENCEGAH ERROR FOLDER READ-ONLY VERCEL (Bypass Cache)
 $_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
 $_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
 $_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 $_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
 $_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
 
-// 2. PAKSA DRIVER AGAR TIDAK KOSONG (Mencegah error createDriver)
-$envs = [
-    'SESSION_DRIVER' => 'cookie',
-    'CACHE_STORE' => 'array',
-    'CACHE_DRIVER' => 'array',
-    'LOG_CHANNEL' => 'stderr',
-    'DB_CONNECTION' => 'sqlite',
-    'DB_DATABASE' => '/tmp/database.sqlite'
-];
-
-foreach ($envs as $key => $value) {
-    putenv($key . '=' . $value);
-    $_ENV[$key] = $value;
-    $_SERVER[$key] = $value;
-}
+// 2. OBAT KUAT: Memaksa Maintenance Driver agar tidak string kosong ('')!
+putenv('APP_MAINTENANCE_DRIVER=file');
+$_ENV['APP_MAINTENANCE_DRIVER'] = 'file';
+$_SERVER['APP_MAINTENANCE_DRIVER'] = 'file';
 
 // 3. PANGGIL LARAVEL
 require __DIR__ . '/../public/index.php';
